adf3fc44aeb98c9546911f8e116840d3807a3e56
thejasree1 <p.n.thejasree1508@gmail.com>
2021-10-18 00:36:18 +0530

    fix: refactor hard coded connection string to Heroku config var

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: 'mongodb+srv://teja:Thejunani15@website.z7bqo.mongodb.net/test?retryWrites=true&w=majority',
-   mongoUri: process.env.DB_CONNECT || 'mongodb+srv://teja:Thejunani15@website.z7bqo.mongodb.net/test?retryWrites=true&w=majority'
+   mongoUri: process.env.DB_CONNECT || "mongodb+srv://teja:Thejunani15@website.z7bqo.mongodb.net/test?retryWrites=true&w=majority"
 }
 
 export default config
